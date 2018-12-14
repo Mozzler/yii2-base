472b9f3bfb0dcd1dd68f33348b1302502e7b4df7
Chris Were <chris@mozzler.com.au>
2018-12-14 23:01:19 +0000

    Add model delete action

---
+++ actions/ModelDeleteAction.php
@@ -0,0 +1,35 @@
+<?php
+namespace mozzler\base\actions;
+
+use yii\helpers\Url;
+
+class ModelDeleteAction extends BaseModelAction
+{
+	public $id = 'delete';
+
+    /**
+     * @var string the name of the view action. This property is need to create the URL when the model is successfully deleted.
+     */
+    public $viewAction = 'index';
+
+    /**
+     * Deletes a model.
+     * @return \yii\db\ActiveRecordInterface the model newly created
+     * @throws ServerErrorHttpException if there is any error when creating the model
+     */
+    public function run()
+    {
+	    $id = \Yii::$app->request->get('id');
+        $model = $this->findModel($id);
+	    
+        if ($this->checkAccess) {
+            call_user_func($this->checkAccess, $this->getUniqueId(), $model);
+        }
+        
+        if ($model && $model->delete()) {
+	        return $this->controller->redirect([$this->viewAction]);
+        }
+
+        return parent::run();
+    }
+}
