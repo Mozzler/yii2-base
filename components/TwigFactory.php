c361fe053a4bccba85736b8117ecd5b71704bd27
Chris Were <chris@mozzler.com.au>
2019-01-03 17:43:09 +0000

    Fix issue with globals not registering correctly

--- components/TwigFactory.php
+++ components/TwigFactory.php
@@ -2,6 +2,7 @@
 namespace mozzler\base\components;
 
 use yii\twig\Twig_Empty_Loader;
+use yii\twig\ViewRendererStaticClassProxy;
 
 class TwigFactory {
 	
@@ -15,12 +16,24 @@ class TwigFactory {
 		$twig->getExtension('Twig_Extension_Core')->setTimezone(\Yii::$app->formatter->timeZone);
 		$twig->getExtension('Twig_Extension_Core')->setDateFormat(\Yii::$app->formatter->dateFormat, '%d days');
 		
-		$twig->addGlobal('html', '\yii\helpers\Html');
-		$twig->addGlobal('arrayhelper', '\yii\helpers\ArrayHelper');
-		$twig->addGlobal('t', '\mozzler\base\components\Tools');
+		$twig = self::addGlobals($twig, [
+			'html' => '\yii\helpers\Html',
+			'arrayhelper' => '\yii\helpers\ArrayHelper',
+			't' => '\mozzler\base\components\Tools'
+		]);
+
 		$twig->addExtension(new \Twig_Extension_StringLoader());
 		
 		return $twig;
+	}
+	
+	protected static function addGlobals($twig, $globals) {
+		foreach ($globals as $name => $className) {
+            $value = new ViewRendererStaticClassProxy($className);
+            $twig->addGlobal($name, $value);
+        }
+        
+        return $twig;
 	}
 	
 }
