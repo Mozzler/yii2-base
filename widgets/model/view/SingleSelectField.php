6a8f5705d85d117aab80d85cf66cde6f25c2c167
Chris Were <chris@mozzler.com.au>
2019-01-11 05:33:07 +0000

    Gracefully handle scenario where attribute doesnt exist

--- widgets/model/view/SingleSelectField.php
+++ widgets/model/view/SingleSelectField.php
@@ -8,11 +8,17 @@ class SingleSelectField extends BaseField {
 	public function config($templatify=true)
 	{
 		$config = parent::config();
-		
 		$modelField = $config['model']->getModelField($config['attribute']);
 		$attribute = $config['attribute'];
 		
-		$config['displayValue'] = $modelField->options[$config['model']->$attribute];
+		if (!isset($modelField->options[$config['model']->$attribute]))
+		{
+    		\Yii::warning("Unable to locate option ".$config['model']->$attribute." for field ".$attribute);
+		}
+		else
+		{
+    		$config['displayValue'] = $modelField->options[$config['model']->$attribute];
+		}
 		
 		return $config;
 	}
