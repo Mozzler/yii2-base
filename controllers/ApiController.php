a13f8fa91a1f308425e0c50185e03f6bbc6e03f0
Michael Kubler <michael@greyphoenix.biz>
2019-02-11 00:00:12 +1030

    * Updated the default RBAC function for the Api Controller. It'll now actually use the $rbacPublicActionsDisabled as defined, plus will default to disabling the default CRUD endpoints for public but allowing them for registered.

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -14,30 +14,92 @@ class ApiController extends BaseController
 
     /**
      * Specify a list of actions that should deny public access
-     * 
-     * ie: ['index', 'view']
+     *
+     * e.g: ['index', 'views', 'create', 'update', 'delete']
      */
-    public static $rbacPublicActionsDisabled = [];
-	
-	// custom serializer to support scenario based responses
-	public $serializer = [
+    public static $rbacPublicActionsDisabled = ['index', 'views', 'create', 'update', 'delete'];
+
+    // custom serializer to support scenario based responses
+    public $serializer = [
         'class' => 'mozzler\base\yii\rest\Serializer',
         'collectionEnvelope' => 'items',
         'itemEnvelope' => 'item'
     ];
-    
-    public static function rbac() {
-        $rbac = [];
 
-        $rbac['public'] = [];
-        foreach (self::$rbacPublicActionsDisabled as $actionName) {
-            $rbac['public'] = [$actionName] = [
+
+    /**
+     * @return array
+     *
+     * By default, don't allow public access, but do allow registered (logged in) access.
+     *
+     * This uses the Controller's public static $rbacPublicActionsDisabled array, which you can adjust accordingly if adding new actions
+     * and want to keep the public locked out but registered (logged in) users being able to access them.
+     *
+     * If you want full control you can manually overwrite this by declaring your own rbac function. e.g
+     *
+     * public static function rbac() {
+     * return [
+     * "public" => [
+     *   'specialActionName' => [
+     *     'grant' => true
+     * ],
+     *   'index' => [
+     *     'grant' => false
+     * ],
+     *   'view' => [
+     *     'grant' => false
+     * ],
+     *   'create' => [
+     *     'grant' => false
+     * ],
+     *   'update' => [
+     *     'grant' => false
+     * ],
+     *   'delete' => [
+     *   'grant' => false
+     * ]],
+     * "registered" => [
+     *   'specialActionName' => [
+     *     'grant' => true
+     * ],
+     *   'index' => [
+     *     'grant' => true
+     * ],
+     *   'view' => [
+     *     'grant' => true
+     * ],
+     *   'create' => [
+     *     'grant' => true
+     * ],
+     *   'update' => [
+     *     'grant' => true
+     * ]
+     *   'delete' => [
+     *     'grant' => true
+     * ]]];
+     * }
+     */
+    public static function rbac()
+    {
+
+        $rbac = [
+            'public' => [],
+            'registered' => [],
+        ];
+
+        $actions = static::$rbacPublicActionsDisabled;
+        foreach ($actions as $actionIndex => $actionName) {
+            $rbac['public'][$actionName] = [
                 'grant' => false
+            ];
+            $rbac['registered'][$actionName] = [
+                'grant' => true
             ];
         }
 
         return $rbac;
-	}
+    }
+
     
     /**
      * @inheritdoc
