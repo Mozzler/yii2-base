47dc95aacb62602b7e6e82f19bdb84898b939e6f
Chris Were <chris@mozzler.com.au>
2019-02-11 17:22:57 +1030

    Initial placeholder for cron structure

---
+++ cron/CronEntry.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace mozzler\base\cron;
+
+class CronEntry extends yii\base\Component
+{
+
+    public $scriptClass;
+
+    public $config = [];
+
+    public $minutes = "*";
+
+    public $hour = "*";
+
+    public $dayMonth = "*";
+
+    public $month = "*";
+
+    public $dayWeek = "*";
+
+    public $timezone = "Australia/Adelaide";
+
+    public $active = false;
+
+}
