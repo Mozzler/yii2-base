60dcbb171b5c87aeb15b438336fb0013e3bf1189
Michael Kubler <michael@greyphoenix.biz>
2019-02-12 21:33:55 +1030

    * Minor fixes.

--- scripts/ScriptBase.php
+++ scripts/ScriptBase.php
@@ -4,7 +4,6 @@ namespace mozzler\base\scripts;
 abstract class ScriptBase {
 
     // This method must be implemented by all scripts
-    public abstract function run() {
-    }
+    public abstract function run();
 
 }
