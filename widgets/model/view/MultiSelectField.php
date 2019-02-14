45479796b7ccae596283e49f37d02a9ea92c7ceb
Chris Were <chris@mozzler.com.au>
2019-02-14 14:09:48 +1030

    Make multi select inherit from single select

--- widgets/model/view/MultiSelectField.php
+++ widgets/model/view/MultiSelectField.php
@@ -3,27 +3,7 @@ namespace mozzler\base\widgets\model\view;
 
 use mozzler\base\widgets\model\view;
 
-class MultiSelectField extends BaseField {
-	
-	public function config($templatify=true)
-	{
-		$config = parent::config();
-		
-		$modelField = $config['model']->getModelField($config['attribute']);
-		$attribute = $config['attribute'];
-		$value = $config['model']->$attribute;
-		
-		if ($value) {
-			$labels = $modelField->getOptionLabels($value);
-			$config['displayValues'] = join($labels, ', ');
-		} else {
-			$config['displayValues'] = '';
-		}
-		
-		
-		
-		return $config;
-	}
+class MultiSelectField extends SingleSelectField {
 	
 }
 
