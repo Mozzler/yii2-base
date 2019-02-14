566f0da29d19720976fae3f34f1893dad26a39fe
Chris Were <chris@mozzler.com.au>
2019-02-14 18:11:34 +1030

    Fix warning message

--- fields/SingleSelect.php
+++ fields/SingleSelect.php
@@ -23,19 +23,14 @@ class SingleSelect extends Base {
 			$options = [$options];
 		}
 
-		\Yii::trace(print_r($options,true));
-		\Yii::trace(print_r($this->options,true));
-
 		$result = [];
 		foreach ($options as $option) {
 			if (isset($this->options[$option])) {
 				$result[] = $this->options[$option];
 			} else {
-				\Yii::warning('Invalid select option specified ('.$option.' for field '.$this->name);
+				\Yii::warning('Invalid select option specified ('.$option.' for field '.$this->attribute.')');
 			}
 		}
-
-		\Yii::trace(print_r($result,true));
 		
 		return $result;
 	}
