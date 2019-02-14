3b5068ceeb07cbfaa37c1c006626b51201f65753
Michael Kubler <michael@greyphoenix.biz>
2019-02-14 14:40:02 +1030

    * First pass of converting the Rappsio JS code for if the Cron should run to PHP.

--- cron/CronEntry.php
+++ cron/CronEntry.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace mozzler\base\cron;
+
 use \yii\base\Component;
+
 class CronEntry extends Component
 {
 
@@ -24,4 +26,59 @@ class CronEntry extends Component
     public $active = false;
 
     public $timeoutSeconds = 120; // In seconds
+
+
+    /**
+     * @param integer $utcUnixTimestamp Unixtimestamp (in seconds)
+     */
+    public function shouldRunCronAtTime($utcUnixTimestamp = null)
+    {
+        if (false === $this->active) {
+            return false;
+        }
+
+
+        // -- Deal with the timezone issues
+        $dateTimeZone = new \DateTimeZone($this->timezone);
+        $date = new \DateTime(null === $utcUnixTimestamp ? "@" . time() : "@" . $utcUnixTimestamp, $dateTimeZone);
+        $thisMinuteTimestamp = round(floor($date->getTimestamp() / 60) * 60);
+
+        // Test interval matches for all intervals
+        var
+        $match = "OK";
+        if (!$this->intervalMatch($this->minutes, $thisMinuteTimestamp, "i")) {
+            $match = "minute";
+        }
+        if (!$this->intervalMatch($this->hours, $thisMinuteTimestamp, "h")) {
+            $match = "hour";
+        }
+        if (!$this->intervalMatch($this->day_month, $thisMinuteTimestamp, "d")) {
+            $match = "day_month";
+        }
+        if (!$this->intervalMatch($this->month, $thisMinuteTimestamp, "m")) {
+            $match = "month";
+        }
+        if (!$this->intervalMatch($this->day_week, $thisMinuteTimestamp, "w")) {
+            $match = "day_week";
+        }
+        if ("OK" === $match) {
+            return true;
+        }
+        \Yii::debug("The shouldRunCronAtTime() is false because the {$match} is incorrect");
+        return false;
+    }
+
+    public function intervalMatch($intervalValue, $timestamp, $intervalFormat)
+    {
+
+        if ($intervalValue === "*") {
+            return true;
+        }
+        $intervalValues = explode(',', $intervalValue);
+        foreach ($intervalValues as $intervalIndex => $interval) {
+            $intervalValues[$intervalIndex] = intval($interval); // Ensure it's a whole number
+        }
+        $currentInterval = intval(date($intervalFormat, $timestamp));
+        return in_array($currentInterval, $intervalValues);
+    }
 }
