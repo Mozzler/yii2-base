0c020cbbf11dcf5179180cc4b45005840b8534f7
Chris Were <chris@mozzler.com.au>
2019-02-19 09:07:29 +1030

    Add support for Boolean input and views

---
+++ widgets/model/view/BooleanField.php
@@ -0,0 +1,18 @@
+<?php
+namespace mozzler\base\widgets\model\view;
+
+use mozzler\base\widgets\model\view;
+use yii\helpers\ArrayHelper;
+
+class BooleanField extends BaseField {
+
+    public function defaultConfig() {
+        return ArrayHelper::merge(parent::defaultConfig(), [
+            'trueValue' => 'True',
+            'falseValue' => 'False'
+        ]);
+    }
+
+}
+
+?>
