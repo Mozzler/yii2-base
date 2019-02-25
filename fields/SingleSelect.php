6205089a59482ce1ac53861a5e6ef7c49025d12a
Michael Kubler <michael@greyphoenix.biz>
2019-02-26 00:25:51 +0800

    * Added a bunch of debugging to the SingleSelect

--- fields/SingleSelect.php
+++ fields/SingleSelect.php
@@ -3,46 +3,52 @@ namespace mozzler\base\fields;
 
 use yii\helpers\ArrayHelper;
 
-class SingleSelect extends Base {
-	
-	public $type = 'SingleSelect';
-	public $operator = "=";
-	public $options = [];
-	
-	public function defaultRules() {
-		return [
-			'string' => [
-				'max' => 255
-			]
-		];
-	}
+class SingleSelect extends Base
+{
 
-	public function rules() {
-		return ArrayHelper::merge(parent::rules(), [
-			'in' => ['range' => array_keys($this->options), 'message' => 'Invalid option specified for '.$this->label]
-		]);
-	}
-	
-	/**
-	 * Take an array of option keys and return the values
-	 */
-	public function getOptionLabels($options=[]) {
-		if (!is_array($options)) {
-			$options = [$options];
-		}
+    public $type = 'SingleSelect';
+    public $operator = "=";
+    public $options = [];
 
-		$result = [];
-		foreach ($options as $option) {
-			if (isset($this->options[$option])) {
-				$result[] = $this->options[$option];
-			} else {
-				\Yii::warning('Invalid select option specified ('.$option.' for field '.$this->attribute.')');
-			}
-		}
-		
-		return $result;
-	}
-	
+    public function defaultRules()
+    {
+        return [
+            'string' => [
+                'max' => 255
+            ]
+        ];
+    }
+
+    public function rules()
+    {
+        \Codeception\Util\Debug::debug("About to check the " . __METHOD__ . " for the options: " . var_export($this->options, true));
+        \Codeception\Util\Debug::debug("Checking that the range is in: " . var_export(array_keys($this->options), true));
+        return ArrayHelper::merge(parent::rules(), [
+            'in' => ['range' => array_keys($this->options), 'message' => 'Invalid option specified for ' . $this->label]
+        ]);
+    }
+
+    /**
+     * Take an array of option keys and return the values
+     */
+    public function getOptionLabels($options = [])
+    {
+        if (!is_array($options)) {
+            $options = [$options];
+        }
+
+        $result = [];
+        foreach ($options as $option) {
+            if (isset($this->options[$option])) {
+                $result[] = $this->options[$option];
+            } else {
+                \Yii::warning('Invalid select option specified (' . $option . ' for field ' . $this->attribute . ')');
+            }
+        }
+
+        return $result;
+    }
+
 }
 
 ?>
