fb28a22dcacd5c76333479644262ae576c1b184b
Michael Kubler <michael@greyphoenix.biz>
2019-03-21 01:01:48 +0800

    * Added the core auditLogBehaviour code

--- models/behaviors/AuditLogBehaviour.php
+++ models/behaviors/AuditLogBehaviour.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace mozzler\base\models\behaviors;
 
 use MongoDB\BSON\ObjectId;
+use mozzler\base\components\Tools;
 use mozzler\base\models\AuditLog;
 use mozzler\base\models\Model;
 use yii\db\BaseActiveRecord;
@@ -14,7 +16,8 @@ use yii\behaviors\AttributesBehavior;
 class AuditLogBehaviour extends AttributesBehavior
 {
 
-    public $actionId = '';
+    public $auditLogActionId = '';
+
     /**
      * {@inheritdoc}
      */
@@ -23,50 +26,63 @@ class AuditLogBehaviour extends AttributesBehavior
         parent::init();
 
 
-        $this->actionId = new ObjectId(); // This should be fairly unique for this action
+        $this->auditLogActionId = new ObjectId(); // This should be fairly unique for this action
         $attributes = [];
 
         // We need to save the auditLog :
         // - after the Insert we know the modelId
         // - before the Update so we know the previous value
-        //- Before the Delete so we know the modelId
-        foreach ($this->attributes() as $attribute) {
+        // - Before the Delete so we know the modelId
+        foreach ($this->owner->attributes() as $attribute) {
             $attributes[$attribute] = [
-                BaseActiveRecord::EVENT_AFTER_INSERT=> [$this, 'saveAuditLog'],
+                BaseActiveRecord::EVENT_AFTER_INSERT => [$this, 'saveAuditLog'],
                 BaseActiveRecord::EVENT_BEFORE_UPDATE => [$this, 'saveAuditLog'],
                 BaseActiveRecord::EVENT_BEFORE_DELETE => [$this, 'saveAuditLog']
             ];
         }
+        $this->attributes = $attributes;
     }
 
     /**
      */
     protected function saveAuditLog($event, $attribute)
     {
-
-
         /** @var Model $model */
         $model = $this->owner;
+
+        $action = $event->name; // Init
+        switch ($event->name) {
+            case BaseActiveRecord::EVENT_AFTER_INSERT:
+                $action = AuditLog::ACTION_INSERT;
+                break;
+            case BaseActiveRecord::EVENT_BEFORE_UPDATE:
+                $action = AuditLog::ACTION_UPDATE;
+                break;
+            case BaseActiveRecord::EVENT_BEFORE_DELETE:
+                $action = AuditLog::ACTION_DELETE;
+                break;
+        }
 
         $auditLogData = [
             'newValue' => $model->$attribute,
             'field' => $attribute,
             'entityId' => $model->getId(),
             'entityType' => get_class($model),
-            'action' => AuditLog::ACTION_UPDATE // @todo: Work this out
+            'action' => $action,
+            'actionId' => $this->auditLogActionId,
         ];
 
 
-    // @todo: Work out the EVENT and if EVENT_BEFORE_UPDATE then get the previous value
-        // locate the previous inserted autoincrement value for this attribute
-//        $models = \Yii::$app->t::getModels($model::className(), [], [
-//            'sort' => [
-//                $attribute => -1
-//            ],
-//            'limit' => 1,
-//            'checkPermissions' => false
-//        ]);
-//
+        if ($action === AuditLog::ACTION_UPDATE)
+            // Locate the previous value for this attribute
+            $previousModel = Tools::getModel($model::className(), ['_id' => Tools::ensureId($model->getId()), false]);
+        if (!empty($previousModel)) {
+            $auditLogData['oldValue'] = $previousModel->$attribute;
+        }
+
+        $auditLog = Tools::createModel(AuditLog::class, $auditLogData);
+        $auditLog->save(true, null, false);
+
         return $this->owner->$attribute; // Return the original attribute
     }
 }
