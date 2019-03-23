0be06f3856b93586e53155aa69430fccf6c04ddd
Michael Kubler <michael@greyphoenix.biz>
2019-03-23 13:09:16 +0800

    * Wrap the auditLog behaviour in a try/catch. Don't want it failing to cause the whole project to stop working.

--- models/behaviors/AuditLogBehaviour.php
+++ models/behaviors/AuditLogBehaviour.php
@@ -27,35 +27,39 @@ class AuditLogBehaviour extends AttributesBehavior
     {
         parent::init();
 
+        try {
 
-        $this->auditLogActionId = (string)new ObjectId(); // This should be fairly unique for this action
-        $attributes = [];
+            $this->auditLogActionId = (string)new ObjectId(); // This should be fairly unique for this action
+            $attributes = [];
 
-        $auditLogAttributes = [];
-        if (!empty($this->auditLogAttributes)) {
-            $auditLogAttributes = $this->auditLogAttributes;
-        } else if (!empty($this->owner) && method_exists($this->owner, 'attributes')) {
-            $auditLogAttributes = $this->owner->attributes();
-        }
-
-        // We need to save the auditLog :
-        // - after the Insert we know the modelId
-        // - before the Update so we know the previous value
-        // - Before the Delete so we know the modelId
-
-
-        if (!empty($auditLogAttributes)) {
-            foreach ($auditLogAttributes as $attribute) {
-                $attributes[$attribute] = [
-                    BaseActiveRecord::EVENT_AFTER_INSERT => [$this, 'saveAuditLog'],
-                    BaseActiveRecord::EVENT_BEFORE_UPDATE => [$this, 'saveAuditLog'],
-                    BaseActiveRecord::EVENT_BEFORE_DELETE => [$this, 'saveAuditLog']
-                ];
+            $auditLogAttributes = [];
+            if (!empty($this->auditLogAttributes)) {
+                $auditLogAttributes = $this->auditLogAttributes;
+            } else if (!empty($this->owner) && method_exists($this->owner, 'attributes')) {
+                $auditLogAttributes = $this->owner->attributes();
             }
+
+            // We need to save the auditLog :
+            // - after the Insert we know the modelId
+            // - before the Update so we know the previous value
+            // - Before the Delete so we know the modelId
+
+
+            if (!empty($auditLogAttributes)) {
+                foreach ($auditLogAttributes as $attribute) {
+                    $attributes[$attribute] = [
+                        BaseActiveRecord::EVENT_AFTER_INSERT => [$this, 'saveAuditLog'],
+                        BaseActiveRecord::EVENT_BEFORE_UPDATE => [$this, 'saveAuditLog'],
+                        BaseActiveRecord::EVENT_BEFORE_DELETE => [$this, 'saveAuditLog']
+                    ];
+                }
 //            \Yii::debug("Setting the AuditLogBehaviour attributes to: " . print_r($attributes, true) . "\nBased on the auditLogAttributes: ". json_encode($auditLogAttributes));
-            $this->attributes = $attributes;
-        } else {
-            \Yii::debug("The AuditLogBehaviour owner isn't set. Can't find the attributes");
+                $this->attributes = $attributes;
+            } else {
+                \Yii::debug("The AuditLogBehaviour owner isn't set. Can't find the attributes");
+            }
+        } catch (\Throwable $exception) {
+            \Yii::error("The AuditLogBehaviour Init errored with: " . $exception->getMessage());
         }
     }
 
@@ -63,55 +67,58 @@ class AuditLogBehaviour extends AttributesBehavior
      */
     protected function saveAuditLog($event, $attribute)
     {
-        // @todo: Only process dirty (changed) attributes if it's on update
-        // @todo: Don't process the createdAt nor updatedAt fields
-        /** @var Model $model */
-        $model = $this->owner;
+        try {
 
-        $action = $event->name; // Init
-        switch ($event->name) {
-            case BaseActiveRecord::EVENT_AFTER_INSERT:
-                $action = AuditLog::ACTION_INSERT;
-                break;
-            case BaseActiveRecord::EVENT_BEFORE_UPDATE:
-                $action = AuditLog::ACTION_UPDATE;
-                break;
-            case BaseActiveRecord::EVENT_BEFORE_DELETE:
-                $action = AuditLog::ACTION_DELETE;
-                break;
-        }
+            // @todo: Only process dirty (changed) attributes if it's on update
+            /** @var Model $model */
+            $model = $this->owner;
 
-        $auditLogData = [
-            'newValue' => json_encode($model->$attribute),
-            'field' => $attribute,
-            'entityId' => Tools::ensureId($model->getId()),
-            'entityType' => get_class($model),
-            'action' => $action,
-            'actionId' => $this->auditLogActionId,
-        ];
+            $action = $event->name; // Init
+            switch ($event->name) {
+                case BaseActiveRecord::EVENT_AFTER_INSERT:
+                    $action = AuditLog::ACTION_INSERT;
+                    break;
+                case BaseActiveRecord::EVENT_BEFORE_UPDATE:
+                    $action = AuditLog::ACTION_UPDATE;
+                    break;
+                case BaseActiveRecord::EVENT_BEFORE_DELETE:
+                    $action = AuditLog::ACTION_DELETE;
+                    break;
+            }
+
+            $auditLogData = [
+                'newValue' => json_encode($model->$attribute),
+                'field' => $attribute,
+                'entityId' => Tools::ensureId($model->getId()),
+                'entityType' => get_class($model),
+                'action' => $action,
+                'actionId' => $this->auditLogActionId,
+            ];
 
 
-        if (AuditLog::ACTION_UPDATE === $action) {
+            if (AuditLog::ACTION_UPDATE === $action) {
 
-            // Locate the previous value for this attribute
-            $previousModel = Tools::getModel($model::className(), ['_id' => Tools::ensureId($model->getId())], false);
-            if (!empty($previousModel)) {
-                $auditLogData['previousValue'] = json_encode($previousModel->$attribute);
+                // Locate the previous value for this attribute
+                $previousModel = Tools::getModel($model::className(), ['_id' => Tools::ensureId($model->getId())], false);
+                if (!empty($previousModel)) {
+                    $auditLogData['previousValue'] = json_encode($previousModel->$attribute);
 
-                if ($auditLogData['previousValue'] === $auditLogData['newValue']) {
-                    return $this->owner->$attribute; // The field hasn't changed, so return the original attribute and don't save this
+                    if ($auditLogData['previousValue'] === $auditLogData['newValue']) {
+                        return $this->owner->$attribute; // The field hasn't changed, so return the original attribute and don't save this
+                    }
                 }
             }
+
+            \Yii::debug("AuditLogBehaviour->saveAuditLog() Setting the AuditLog to to: " . print_r($auditLogData, true));
+
+            $auditLog = Tools::createModel(AuditLog::class, $auditLogData);
+            $auditLogSaved = $auditLog->save(true, null, false);
+            if (!$auditLogSaved) {
+                \Yii::error("auditLog save error:\n" . print_r($auditLog->getErrors(), true));
+            }
+        } catch (\Throwable $exception) {
+            \Yii::error("The AuditLogBehaviour saveAuditLog() errored with: " . $exception->getMessage());
         }
-
-        \Yii::debug("AuditLogBehaviour->saveAuditLog() Setting the AuditLog to to: " . print_r($auditLogData, true));
-
-        $auditLog = Tools::createModel(AuditLog::class, $auditLogData);
-        $auditLogSaved = $auditLog->save(true, null, false);
-        if (!$auditLogSaved) {
-            \Yii::error("auditLog save error:\n" . print_r($auditLog->getErrors(), true));
-        }
-
         return $this->owner->$attribute; // Return the original attribute
     }
 }
