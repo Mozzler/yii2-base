36436a70387b980afe65b96df1ae42a88667a6b0
Michael Kubler <michael@greyphoenix.biz>
2019-04-02 11:01:22 +0800

    * Renaming the Ahref a LinkField and refactoring it as per the usual way - https://trello.com/c/9X5Srk6f/270-acp-fix-faq-wrapping

---
+++ widgets/model/view/LinkField.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace mozzler\base\widgets\model\view;
+
+use mozzler\base\widgets\model\view;
+use yii\helpers\ArrayHelper;
+
+class LinkField extends BaseField
+{
+    /*
+     * Example model usage. On a model:
+     * public function modelFields() { return  [
+     * 'answerButtonLink' => [
+            'type' => 'Text',
+            'label' => 'Link',
+            'required' => false,
+            'widgets' => [
+                'view' => [
+                    'class' => 'mozzler\base\widgets\model\view\LinkField',
+                    'options' => [
+                        'title' => 'Answer Link',
+                        'target' => '_self',
+                    ]
+                ]
+            ]
+    ];}
+    */
+    public function defaultConfig()
+    {
+        return \yii\helpers\ArrayHelper::merge(parent::defaultConfig(), [
+            "tag" => "a",
+            "options" => [
+                "class" => "",
+                "href" => "{{ widget.model[widget.attribute] }}",
+                "title" => '',
+                "target" => '_blank',
+            ],
+            "model" => null,
+            "attribute" => null
+        ]);
+    }
+
+    public function config($templatify = true)
+    {
+        $config = parent::config(true);
+        return $config;
+    }
+}
+
+?>
