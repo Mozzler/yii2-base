b7c24c424f9ff113ebef348bb4305f175c3aad43
kublermdk <michael@filmsonthefly.com>
2019-04-10 19:22:43 +0800

    * An update action should now return the VIEW_API or VIEW Scenario as appropriate.

--- actions/ActiveUpdateAction.php
+++ actions/ActiveUpdateAction.php
@@ -6,6 +6,6 @@ class ActiveUpdateAction extends \yii\rest\UpdateAction
 {
 
     public $scenario = [Model::SCENARIO_UPDATE_API, Model::SCENARIO_UPDATE];
-    public $resultScenario = [Model::SCENARIO_UPDATE_API, Model::SCENARIO_UPDATE];
+    public $resultScenario = [Model::SCENARIO_VIEW_API, Model::SCENARIO_VIEW];
     
 }
