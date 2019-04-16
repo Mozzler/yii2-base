c03c3c526f82673b0337cfd62860cfeb9ab4e764
Chris Were <chris@mozzler.com.au>
2019-04-16 13:29:57 +0930

    Make timestamp adhere to timezone

--- widgets/model/view/TimestampField.php
+++ widgets/model/view/TimestampField.php
@@ -2,7 +2,17 @@
 namespace mozzler\base\widgets\model\view;
 
 use mozzler\base\widgets\model\view;
+use yii\helpers\ArrayHelper;
 
-class TimestampField extends BaseField {}
+class TimestampField extends BaseField {
+
+    public function defaultConfig() {
+        return ArrayHelper::merge(parent::defaultConfig(), [
+            'format' => \Yii::$app->formatter->datetimeFormat,
+            'timeZone' => \Yii::$app->formatter->timeZone
+        ]);
+    }
+
+}
 
 ?>
