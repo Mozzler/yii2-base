06e0171bc3b9a79abe970b9e0b98a274a40c5237
kublermdk <michael@filmsonthefly.com>
2019-04-29 22:39:37 +0800

    * Updated the CurrencyField to use the Yii Formatter.

--- widgets/model/view/CurrencyField.php
+++ widgets/model/view/CurrencyField.php
@@ -1,17 +1,20 @@
 <?php
+
 namespace mozzler\base\widgets\model\view;
 
 use mozzler\base\widgets\model\view;
+use yii\helpers\ArrayHelper;
 
-class CurrencyField extends BaseField {
+class CurrencyField extends BaseField
+{
     public function defaultConfig()
     {
         return \yii\helpers\ArrayHelper::merge(parent::defaultConfig(), [
             "tag" => "span",
-            "currencySymbol" => "$", // @todo: Make this automatically locale specific
-            "decimalPlaces" => 2, // How many decimals to show
-            "decimalSeparator" => '.',
-            "thousandsSeparator" => ',',
+            "currencySymbol" => null, // @todo: Make this automatically locale specific
+            "numberFormatterOptions" => [], // How many decimals to show example: [ \NumberFormatter::MIN_FRACTION_DIGITS => 0, \NumberFormatter::MAX_FRACTION_DIGITS => 2,
+        ],
+            "numberFormatterTextOptions" => [],
             "options" => [
                 "class" => "",
             ],
@@ -23,6 +26,11 @@ class CurrencyField extends BaseField {
     public function config($templatify = true)
     {
         $config = parent::config(true);
+
+        if (!empty($config['attribute']) && !empty($config['model'])) {
+            $attributeValue = ArrayHelper::getValue($config, 'model.' . $config['attribute']);
+            $config['formattedValue'] = \Yii::$app->formatter->asCurrency($attributeValue, $config['currencySymbol'], $config['numberFormatterOptions'], $config['numberFormatterTextOptions']);
+        }
         return $config;
     }
 
