6489f0cc41d74a8b8eadfd86118c36477d4c4d2b
kublermdk <michael@filmsonthefly.com>
2019-08-28 20:01:05 +0800

    * Start of the work on the SystemLog and SystemLogTarget.

---
+++ log/SystemLogTarget.php
@@ -0,0 +1,95 @@
+<?php
+
+
+namespace mozzler\base\log;
+use app\components\GeneralTools;
+use mozzler\base\components\Tools;
+use mozzler\base\models\SystemLog;
+use Yii;
+use yii\log\Target;
+
+/**
+ * Class SystemLogTarget
+ * @package mozzler\base\log
+ */
+class SystemLogTarget extends Target
+{
+
+    /**
+     * Initializes the route.
+     * This method is invoked after the route is created by the route manager.
+     */
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function export()
+    {
+        $text = implode("\n", array_map([$this, 'formatMessage'], $this->messages)) . "\n";
+
+        $summary = $this->collectRequest();
+        $systemLog = Tools::createModel(SystemLog::class, [
+
+            'request' => $this->collectRequest()
+
+        ]);
+    }
+
+
+    /**
+     * Collects summary data of current request.
+     * @return array
+     */
+    protected function collectRequest()
+    {
+        if (Yii::$app === null) {
+            return [];
+        }
+
+        $request = Yii::$app->getRequest();
+        $response = Yii::$app->getResponse();
+
+        $senderIp = $request->getUserIP(); // Most likely a proxy server
+        $userIP = self::getRealIpAddr(); // Most likely the actual user's IP
+        $summary = [
+            'tag' => $this->tag,
+            'url' => $request->getAbsoluteUrl(),
+            'ajax' => (int) $request->getIsAjax(),
+            'method' => $request->getMethod(),
+            'userAgent' => $request->getUserAgent(),
+            'absoluteUrl' => $request->getAbsoluteUrl(),
+            'bodyParams' => self::sanitiseParamsPassword($request->getBodyParams()),
+            'userIP' => ($senderIp === $userIP) ? $userIP : "{$userIP}, {$senderIp}", // More likely to show the actual users IP address first, then the proxy server,
+            'time' => $_SERVER['REQUEST_TIME_FLOAT'],
+            'headers' => json_encode($request->getHeaders()),
+            'statusCode' => $response->statusCode,
+        ];
+
+        return $summary;
+    }
+
+
+    /**
+     * @param $params
+     * @return array
+     */
+    public static function sanitiseParamsPassword($params)
+    {
+        if (empty($params)) {
+            return $params;
+        }
+        if (!empty($params['password'])) {
+            $params['password'] = "*****";
+        }
+        if (!empty($params['passwordHash'])) {
+            $params['passwordHash'] = "*******";
+        }
+        return $params;
+    }
+
+    protected function getTrace() {
+
+//        $this->getTraceAsString()
+    }
+}
---
+++ models/SystemLog.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace mozzler\base\models;
+
+use mozzler\base\models\Model as BaseModel;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Class SystemLog
+ * @package mozzler\base\models
+ */
+class SystemLog extends BaseModel
+{
+    protected static $collectionName = 'app.systemLog';
+    public const TYPE_DEBUG = 'debug';
+    public const TYPE_INFO = 'info';
+    public const TYPE_WARNING = 'warning';
+    public const TYPE_ERROR = 'error';
+    public const TYPE_EXCEPTION = 'exception';
+
+    protected function modelConfig()
+    {
+        return [
+            'label' => 'System Log',
+            'labelPlural' => 'Sys Log Entries',
+        ];
+    }
+
+    public function modelIndexes()
+    {
+        return ArrayHelper::merge(parent::modelIndexes(), [
+            'createdAt' => [
+                'columns' => ['createdAt' => 1],
+            ],
+            'type' => [
+                'columns' => ['type' => 1],
+            ]
+        ]);
+    }
+
+    protected function modelFields()
+    {
+        return ArrayHelper::merge(parent::modelFields(), [
+            'type' => [
+                'type' => 'SingleSelect',
+                'label' => 'Type',
+                'options' => [
+                    self::TYPE_DEBUG => 'Debug',
+                    self::TYPE_INFO => 'Info',
+                    self::TYPE_WARNING => 'Warning',
+                    self::TYPE_ERROR => 'Error',
+                    self::TYPE_EXCEPTION => 'Exception',
+                ],
+                'default' => self::TYPE_ERROR,
+                'required' => true
+            ],
+            'message' => [
+                'type' => 'TextLarge',
+                'label' => 'Message',
+                'required' => true,
+                'widgets' => [
+                    'view' => [
+                        'class' => 'mozzler\base\widgets\model\view\PreField',
+                    ]
+                ]
+            ],
+            'request' => [
+                'type' => 'Json',
+                'label' => 'Request',
+                'required' => true,
+                'widgets' => [
+                    'view' => [
+                        'class' => 'mozzler\base\widgets\model\view\PreField',
+                    ]
+                ]
+            ],
+            'response' => [
+                'type' => 'Json',
+                'label' => 'Request',
+                'widgets' => [
+                    'view' => [
+                        'class' => 'mozzler\base\widgets\model\view\PreField',
+                    ]
+                ]
+            ],
+            'data' => [
+                'type' => 'Json',
+                'label' => 'Request',
+                'widgets' => [
+                    'view' => [
+                        'class' => 'mozzler\base\widgets\model\view\PreField',
+                    ]
+                ]
+            ],
+            'code' => [
+                'type' => 'Integer',
+                'label' => 'Code',
+                'required' => false
+            ],
+            'namespace' => [
+                'type' => 'Text',
+                'label' => 'Namespace',
+                'required' => false,
+                'widgets' => [
+                    'view' => [
+                        'class' => 'mozzler\base\widgets\model\view\CodeField',
+                    ]
+                ]
+            ],
+        ]);
+    }
+
+
+    /**
+     * At any level of authorisation you should be able to create a new SystemLog entry
+     */
+    public static function rbac()
+    {
+        return ArrayHelper::merge(parent::rbac(), [
+            'registered' => [
+                'insert' => [
+                    'grant' => true
+                ]
+            ],
+            'public' => [
+                'insert' => [
+                    'grant' => true
+                ]
+            ],
+            'admin' => [
+                'insert' => [
+                    'grant' => true
+                ],
+                'view' => [
+                    'grant' => true
+                ],
+                'update' => [
+                    'grant' => true
+                ],
+                'delete' => [
+                    'grant' => true
+                ],
+            ],
+        ]);
+    }
+
+    public function scenarios()
+    {
+        $scenarios = parent::scenarios();
+        $scenarios[self::SCENARIO_CREATE] = ['type', 'message', 'code', 'request', 'namespace'];
+        $scenarios[self::SCENARIO_UPDATE] = $scenarios[self::SCENARIO_CREATE];
+        $scenarios[self::SCENARIO_LIST] = ['type', 'code', 'namespace', 'createdAt'];
+        $scenarios[self::SCENARIO_VIEW] = ['type', 'code', 'message', 'request', 'namespace', 'createdUserId', 'createdAt', 'updatedUserId', 'updatedAt', '_id'];
+        $scenarios[self::SCENARIO_SEARCH] = ['type', 'code', 'namespace', 'message'];
+
+        return $scenarios;
+    }
+
+}
