1beb25207f88309d179029ae16e8ec192ae78325
kublermdk <michael@filmsonthefly.com>
2019-12-13 16:02:55 +0800

    * Removed debugging

--- widgets/model/input/FileField.php
+++ widgets/model/input/FileField.php
@@ -22,12 +22,6 @@ class FileField extends BaseField
         $attribute = $config['attribute'];
 
 
-        \Yii::debug("The filefield config is: " . json_encode($config));
-        \Yii::debug("The model $attribute is set to: " . json_encode($model->$attribute));
-
-        //@todo: Need to show if a file has already been uploaded or not (esp in the update view as it'll cause the entry to be lost)
-
-
         // JS
         $view = \Yii::$app->controller->getView();
         $view->registerJsFile('https://unpkg.com/filepond/dist/filepond.min.js', ['position' => WebView::POS_END, 'depends' => ['yii\web\JqueryAsset']], 'filepond-main');
@@ -71,7 +65,6 @@ if (typeof FilePond === "undefined") {
 	$mozzlerFilePond.each(function(index, element) {
 
 	if (element.value) {
-	 console.log("There\'s already been a file uploaded");
 	 filePond = FilePond.create(element, {
 	 	\'files\': [
 	        {
@@ -89,20 +82,14 @@ if (typeof FilePond === "undefined") {
 	 filePond = FilePond.create(element);
 	}
 	filePonds.push({\'element\': element, \'filePond\': filePond });
-
 	});
 
 	window.mozzler_filePonds = filePonds; // Make global var so devs can make their own changes if needed
-
 }
-
-
-
-
-        ', WebView::POS_READY, 'filepond-setup');
+', WebView::POS_READY, 'filepond-setup');
 
         /** @var \yii\widgets\ActiveField $field */
-        $field = $config['form']->field($config['model'], $config['attribute'], ArrayHelper::merge($config['widgetConfig'], ['inputOptions' => ['value' => (string)$model->$attribute]]));
+        $field = $config['form']->field($config['model'], $config['attribute'], ArrayHelper::merge($config['widgetConfig'], ['inputOptions' => ['value' => (string)$model->$attribute]])); // Ensure the value is saved as a normal string not oid reference
         return $field->hiddenInput(ArrayHelper::merge($config['widgetConfig'], ['class' => 'mozzler-filepond-fileinput'])); // 'mozzler-filepond-fileinput' is what we'll use for triggering the filepond uploader
     }
 
