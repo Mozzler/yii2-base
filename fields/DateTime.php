becb02a350b1bd9e6c99c4a3958bc6963c6b1ee3
kublermdk <michael@filmsonthefly.com>
2020-03-05 16:35:08 +0800

    * Allow sending a DateTime to the DateTime field and having it be converted to a unix timestamp

--- fields/DateTime.php
+++ fields/DateTime.php
@@ -1,35 +1,44 @@
 <?php
+
 namespace mozzler\base\fields;
 
-class DateTime extends Base {
-	
-	public $type = 'DateTime';
-	
-	/**
-	 * Parse a given time value, being timezone aware, into a unix
-	 * epoch integer
-	 */
-	public function setValue($value) {
-		if (!$value) {
-			return null;
-		}
+class DateTime extends Base
+{
 
-		// handle a string value
-		if (is_string($value)) {
-			// if $value is a string containing an integer, return the integer
-			if (strval(intval($value)) == $value) {
-				return intval($value);
-			}
-			
-			$timezone = new \DateTimeZone(\Yii::$app->formatter->timeZone);
-			$epoch = (new \DateTime($value, $timezone))->format("U");
+    public $type = 'DateTime';
 
-			return intval($epoch);
-		}
-		
-		return parent::setValue($value);
-	}
-	
+    /**
+     * Parse a given time value, being timezone aware, into a unix
+     * epoch integer
+     */
+    public function setValue($value)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        // -- Handle a DateTime
+        if ($value instanceof \DateTime) {
+            // This assumes you've got the correct timezone
+            $epoch = $value->format("U");
+            return intval($epoch);
+        }
+
+        // -- Handle a string value
+        if (is_string($value)) {
+            // if $value is a string containing an integer, return the integer
+            if (strval(intval($value)) == $value) {
+                return intval($value);
+            }
+
+            $timezone = new \DateTimeZone(\Yii::$app->formatter->timeZone);
+            $epoch = (new \DateTime($value, $timezone))->format("U");
+
+            return intval($epoch);
+        }
+
+        // We want an integer
+        return intval($value);
+    }
+
 }
-
-?>
