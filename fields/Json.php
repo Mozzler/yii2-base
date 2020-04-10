05c4fef2b9c51375247480a020238f37402ce6fd
kublermdk <michael@filmsonthefly.com>
2020-04-10 15:04:17 +0800

    * Automatically convert JSON encoded strings into actual arrays

--- fields/Json.php
+++ fields/Json.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace mozzler\base\fields;
 
 class Json extends Base
@@ -7,4 +8,23 @@ class Json extends Base
     public $type = 'Json';
     public $filterType = "LIKE";
 
+    /**
+     * @param $value array|string an existing array or JSON string
+     * @return array|mixed
+     *
+     * Automatically converts a JSON string to an actual array
+     */
+    public function setValue($value)
+    {
+        if (empty($value)) {
+            return $value;
+        }
+        if (is_string($value)) {
+            $jsonDecoded = json_decode($value, true);
+            if (is_array($jsonDecoded)) {
+                $value = $jsonDecoded;
+            }
+        }
+        return $value;
+    }
 }
