1612a54bfad2a5ea916112334585b854d77c4a10
kublermdk <michael@filmsonthefly.com>
2020-04-21 21:44:49 +0800

    * The hidden input fields were erroring when trying to output an Array. Now JSON encoding arrays so it shouldn't error and should allow Json and JsonArray model fields to work as hidden inputs

--- widgets/model/input/HiddenField.php
+++ widgets/model/input/HiddenField.php
@@ -1,6 +1,27 @@
 <?php
+
 namespace mozzler\base\widgets\model\input;
+
+use mozzler\base\helpers\WidgetHelper;
+use yii\helpers\ArrayHelper;
 
 class HiddenField extends BaseField
 {
+
+    public function config($templatify = false)
+    {
+        $config = ArrayHelper::merge($this->defaultConfig(), $this->config);
+
+        // We get array to string conversions when trying to process array types as hidden fields, so this JSON encodes them
+        if (!empty($config['model']) && !empty($config['attribute']) && is_array($config['model']->__get($config['attribute']))) {
+            $config['fieldOptions']['value'] = json_encode($config['model']->__get($config['attribute']));
+        }
+        if ($templatify) {
+            $config = WidgetHelper::templatifyConfig($config, ['widget' => $config]);
+        }
+
+        $config['id'] = $this->id;
+
+        return $config;
+    }
 }
