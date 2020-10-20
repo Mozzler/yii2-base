60b74a9635840f5c92dd9bbb79e8fead0811618e
Michael Kubler <michael@greyphoenix.biz>
2020-10-21 01:08:27 +0800

    * Allowed the dataProviderConfig to be set in a model index action (e.g for changing the way the keys are defined)

--- actions/ModelIndexAction.php
+++ actions/ModelIndexAction.php
@@ -7,37 +7,38 @@ use mozzler\base\models\Model;
 
 class ModelIndexAction extends BaseModelAction
 {
-	public $id = 'index';
-    
+    public $id = 'index';
+
     /**
      * @var string the scenario to be assigned to the new model before it is validated and saved.
      */
 
     public $scenario = Model::SCENARIO_LIST;
-    
+    public $dataProviderConfig = [];
+
     public function defaultConfig() {
-	    return ArrayHelper::merge(parent::defaultConfig(),[
-		    'widgetConfig' => []
-	    ]);
+        return ArrayHelper::merge(parent::defaultConfig(),[
+            'widgetConfig' => []
+        ]);
     }
 
     /**
      */
     public function run()
-    {   
+    {
         $model = $this->controller->getModel();
         $model->setScenario($this->scenario);
-        
+
         $rbacFilter = \Yii::$app->rbac->canAccessAction($this);
-        $dataProvider = $model->search(\Yii::$app->request->get(), null, $rbacFilter ? $rbacFilter : null);
+        $dataProvider = $model->search(\Yii::$app->request->get(), null, $rbacFilter ? $rbacFilter : null, $this->dataProviderConfig);
 
         $this->controller->templateData['dataProvider'] = $dataProvider;
-		$this->controller->templateData['model'] = $model;
+        $this->controller->templateData['model'] = $model;
 
-		if ($this->controller->jsonRequested) {
-			$this->controller->data['items'] = $dataProvider->getModels();
-		}
-        
+        if ($this->controller->jsonRequested) {
+            $this->controller->data['items'] = $dataProvider->getModels();
+        }
+
         return parent::run();
     }
 
