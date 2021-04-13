e51244f3720741313d9512e7aea9d03f1b00bff9
Michael Kubler <michael@greyphoenix.biz>
2021-04-13 21:31:30 +0800

    * Removed the "Today" selector from the Date Time range filter but added this week, this year and last year

--- widgets/model/filter/DateTimeField.php
+++ widgets/model/filter/DateTimeField.php
@@ -20,10 +20,13 @@ class DateTimeField extends BaseField
                 'pluginOptions' => [
 //                    'locale' => ['format' => 'Y-m-d']
                     'ranges' => [
-                        "Today" => ["moment().startOf('day')", "moment()"],
+//                        "Today" => ["moment().startOf('day')", "moment().endOf('day')"], // There's a bug where this doesn't work when first clicked, you have to select a different range, then select today. It seems to be an annoying bug
                         "Yesterday" => ["moment().startOf('day').subtract(1,'days')", "moment().endOf('day').subtract(1,'days')"],
+                        "This Week" => ["moment().startOf('week')", "moment().endOf('day')"],
                         "This Month" => ["moment().startOf('month')", "moment().endOf('month')"],
                         "Last Month" => ["moment().subtract(1, 'month').startOf('month')", "moment().subtract(1, 'month').endOf('month')"],
+                        "This Year" => ["moment().startOf('year')", "moment().endOf('day')"],
+                        "Last Year" => ["moment().subtract(1, 'year').startOf('year')", "moment().subtract(1, 'year').endOf('year')"],
                     ]
                 ],
             ]
