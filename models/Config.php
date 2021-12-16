7bad0a3ebf3049af986e92a7f5b97bce9f1af082
Michael Kubler <michael@greyphoenix.biz>
2021-12-17 00:16:23 +0800

    * Added the updatedAt and updated by  to the Config list menu

--- models/Config.php
+++ models/Config.php
@@ -18,55 +18,6 @@ class Config extends BaseModel
 
     protected static $collectionName = 'app.config';
 
-    protected function modelConfig()
-    {
-        return [
-            'label' => 'Config',
-            'labelPlural' => 'Configs'
-        ];
-    }
-
-    public function modelIndexes()
-    {
-        return ArrayHelper::merge(parent::modelIndexes(), [
-            'uniqueKey_' => [
-                'columns' => ['key_' => 1],
-                'options' => [
-                    'unique' => 1
-                ],
-                'duplicateMessage' => ['Config key already exists']
-            ]
-        ]);
-    }
-
-    protected function modelFields()
-    {
-        return ArrayHelper::merge(parent::modelFields(), [
-            'key_' => [
-                'type' => 'Text',
-                'label' => 'Key',
-                'required' => true
-            ],
-            'value_' => [
-                'type' => 'Text',
-                'label' => 'Value',
-                'required' => true
-            ]
-        ]);
-    }
-
-    public function scenarios()
-    {
-        $scenarios = parent::scenarios();
-        $scenarios[self::SCENARIO_CREATE] = ['key_', 'value_'];
-        $scenarios[self::SCENARIO_UPDATE] = $scenarios[self::SCENARIO_CREATE];
-        $scenarios[self::SCENARIO_LIST] = ['key_', 'value_', 'createdAt'];
-        $scenarios[self::SCENARIO_VIEW] = ['key_', 'value_', 'createdAt'];
-        $scenarios[self::SCENARIO_SEARCH] = ['key_'];
-
-        return $scenarios;
-    }
-
     public static function rbac()
     {
         return ArrayHelper::merge(parent::rbac(), [
@@ -94,6 +45,55 @@ class Config extends BaseModel
                 'delete' => [
                     'grant' => true
                 ]
+            ]
+        ]);
+    }
+
+    public function modelIndexes()
+    {
+        return ArrayHelper::merge(parent::modelIndexes(), [
+            'uniqueKey_' => [
+                'columns' => ['key_' => 1],
+                'options' => [
+                    'unique' => 1
+                ],
+                'duplicateMessage' => ['Config key already exists']
+            ]
+        ]);
+    }
+
+    public function scenarios()
+    {
+        $scenarios = parent::scenarios();
+        $scenarios[self::SCENARIO_CREATE] = ['key_', 'value_'];
+        $scenarios[self::SCENARIO_UPDATE] = $scenarios[self::SCENARIO_CREATE];
+        $scenarios[self::SCENARIO_LIST] = ['key_', 'value_', 'createdAt', 'updatedAt', 'updatedUserId'];
+        $scenarios[self::SCENARIO_VIEW] = ['key_', 'value_', 'createdAt', 'updatedAt', 'createdUserId', 'updatedUserId'];
+        $scenarios[self::SCENARIO_SEARCH] = ['key_', 'createdAt', 'updatedAt'];
+
+        return $scenarios;
+    }
+
+    protected function modelConfig()
+    {
+        return [
+            'label' => 'Config',
+            'labelPlural' => 'Configs'
+        ];
+    }
+
+    protected function modelFields()
+    {
+        return ArrayHelper::merge(parent::modelFields(), [
+            'key_' => [
+                'type' => 'Text',
+                'label' => 'Key',
+                'required' => true
+            ],
+            'value_' => [
+                'type' => 'Text',
+                'label' => 'Value',
+                'required' => true
             ]
         ]);
     }
