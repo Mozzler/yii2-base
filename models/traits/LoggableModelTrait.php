d8036f165e611edfcd288974bc0fd23585ee0b3b
Michael Kubler <michael@greyphoenix.biz>
2022-03-20 16:45:50 +0800

    * Allow a max number of Log entries and by default trim the new debug and then info entries, oldest first

--- models/traits/LoggableModelTrait.php
+++ models/traits/LoggableModelTrait.php
@@ -3,13 +3,21 @@
 namespace mozzler\base\models\traits;
 
 
+use yii\helpers\ArrayHelper;
+
 trait LoggableModelTrait
 {
 
     // -- Traits can't have const so using vars and adding the _ for reduced namespace collision chance
     public $_LOG_TYPE_WARNING = 'warning';
     public $_LOG_TYPE_INFO = 'info';
+    public $_LOG_TYPE_DEBUG = 'debug';
     public $_LOG_TYPE_ERROR = 'error';
+
+//    /**
+//     * @var int|null
+//     */
+//    public $_MAX_LOG_ENTRIES = 0; // If set, then we also cull the array to be at most this many entries.
 
 
     /**
@@ -29,6 +37,12 @@ trait LoggableModelTrait
             'message' => $message,
             'type' => $type
         ];
+
+        // -- Allow trimming the log entries to a maximum size
+        if (isset($this->_MAX_LOG_ENTRIES) && $this->_MAX_LOG_ENTRIES > 0 && count($log) > $this->_MAX_LOG_ENTRIES) {
+            // -- Get the X last most entries
+            $log = $this->_trimLog($log, $this->_MAX_LOG_ENTRIES); // By default we remove the debug and info entries before removing the warning and error ones and trim the oldest (earlier in the array) entries, but you can replace this with your own implementation
+        }
         $this->log = $log;
 
         if ($type === $this->_LOG_TYPE_ERROR) {
@@ -38,6 +52,53 @@ trait LoggableModelTrait
         } else {
             \Yii::info($message);
         }
+    }
+
+//    /**
+//     * @param $log array
+//     * @param $maxLogSize int
+//     *
+//     * This trim log function will simply remove older entries (because of the - (negative) to the max log size on the array_slice
+//     */
+//    public function _trimLog($log, $maxLogSize)
+//    {
+//        return array_slice($log, -$maxLogSize); // If offset is negative, the sequence will start that far from the end of the array.
+//    }
+
+    /**
+     * @param $log array
+     * @param $maxLogSize int
+     * @return array
+     *
+     * This trim log function is defined separately in case you want to do more work to selectively remove certain, more noisy log entries whilst keeping more useful ones
+     * Or you want to instead use the more naive approach of just trimming the earliest (or latest or whatever you want)
+     */
+    public function _trimLog($log, $maxLogSize)
+    {
+        $overByCount = count($log) - $maxLogSize;
+        if ($overByCount > 0) {
+            // -- First, we clear out DEBUG entries (which are the access logs, like "Someone viewed the Client Payment Page")
+            $log = array_filter($log, function ($logEntry) use (&$overByCount) {
+                if ($overByCount > 0 && ArrayHelper::getValue($logEntry, 'type') === $this->_LOG_TYPE_DEBUG) {
+                    $overByCount--;
+                    return false;
+                }
+                return true;
+            });
+            $overByCount = count($log) - $maxLogSize; // Recount probably not needed?
+            if ($overByCount > 0) {
+                // Also remove whatever the oldest info entries are as required (hopefully we don't need to do this)
+                $log = array_filter($log, function ($logEntry) use (&$overByCount) {
+                    if ($overByCount > 0 && ArrayHelper::getValue($logEntry, 'type') === $this->_LOG_TYPE_INFO) {
+                        $overByCount--;
+                        return false;
+                    }
+                    return true;
+                });
+            }
+        }
+        // -- Then we remove any other entries, from the top, leaving the most recent at the bottom
+        return array_slice($log, -$maxLogSize); // If offset is negative, the sequence will start that far from the end of the array.
     }
 
     public function returnLogLines()
